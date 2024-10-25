5ee71c3ad48b9df3ab298226c61334ab64e71bbc
cyin-star <65860847+cyin-star@users.noreply.github.com>
2024-10-25 14:37:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,29 @@ async def on_message(message):
             await message.channel.send('ERROR')
 
 
+    if message.content.startswith('$ban'):
+        member = message.author
+        target_role = discord.utils.get(member.guild.roles, name=target_role_name)
+        if target_role not in member.roles:
+            await message.channel.send(f'{member.mention} does not have the required role to use this command.')
+            return
+        m = message.content.split(' ')
+        ip = m[1]
+        payload = {'ip': ip}
+        url = "https://meeatchicken.pythonanywhere.com/unban/bot"
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(url, headers=headers, json=payload)
+        if response.status_code == 200:
+            print("Request successful!")
+            print(response.text)
+            await message.channel.send(f'{response.text}')
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            await message.channel.send('ERROR')
+
+
 try:
   token = os.getenv("TOKEN") or ""
   if token == "":
